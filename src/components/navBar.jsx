1b808ba2cba32b15e9ce4c078f852cd25052e417
Vikramjeet Singh <vikramjeetsingh@Vikramjeets-MacBook-Air.local>
2021-12-29 13:32:19 +0530

    updated app name

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Vidly
+        Merabuddy
       </Link>
       <button
         className="navbar-toggler"
